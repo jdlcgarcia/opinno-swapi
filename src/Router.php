c7612bed31ac5153d609cf71ba88975e023a0f58
jdlcgarcia <kyomuga@gmail.com>
2019-08-16 21:05:31 +0200

    added route for Error

--- src/Router.php
+++ src/Router.php
@@ -6,7 +6,7 @@ abstract class Router
 {
     const BASE_CONTROLLER = '\OpinnoSwapi\controller\\';
     const ALLOWED_ROUTES = [
-        "", "/", "film"
+        "", "/", "film", "error"
     ];
     const SUFFIX_CONTROLLER = 'Controller';
     const SUFFIX_ACTION = "Action";
@@ -14,10 +14,10 @@ abstract class Router
     public static function route()
     {
         $parsedRequestQuery = self::parseParameters();
-        if (self::allowedRoute($parsedRequestQuery[0])) {
-            self::loadControllerAction($parsedRequestQuery);
+        if (!self::allowedRoute($parsedRequestQuery[0])) {
+            $parsedRequestQuery[0] = "error";
         }
-
+        self::loadControllerAction($parsedRequestQuery);
     }
 
     /**
