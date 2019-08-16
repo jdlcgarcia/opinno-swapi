ae77fe1457fcef713cac367f8ed244bfc4787824
jdlcgarcia <kyomuga@gmail.com>
2019-08-16 20:18:30 +0200

    added action that handles the new feature

--- src/controller/IndexController.php
+++ src/controller/IndexController.php
@@ -12,11 +12,20 @@ class IndexController
      */
     public function indexAction()
     {
-//        $swapi = new Swapi();
-//        $json = $swapi->getPerson(1);
         $service = new FilmService();
         $filmList = $service->getFilmList("of");
         $view = "films";
+        require_once("view/template.php");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function filmAction()
+    {
+        $service = new FilmService();
+        $film = $service->getFilm("1");
+        $view = "detail";
         require_once("view/template.php");
     }
 }
