7422a8403237bf31d0712283d55e1181fa408f2f
jdlcgarcia <kyomuga@gmail.com>
2019-08-16 20:03:41 +0200

    model stubs

---
+++ src/model/Planet.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace OpinnoSwapi\model;
+
+
+class Planet
+{
+    /** @var string */
+    private $url;
+
+    /**
+     * Planet constructor.
+     * @param string $url
+     */
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+
+}
