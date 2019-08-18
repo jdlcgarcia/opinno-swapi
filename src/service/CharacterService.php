b5246d6209249f325740f27897006a403ea11964
jdlcgarcia <kyomuga@gmail.com>
2019-08-18 02:25:45 +0200

    service to retrieve characters from Swapi

---
+++ src/service/CharacterService.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace OpinnoSwapi\service;
+
+
+use Exception;
+use OpinnoSwapi\model\Character;
+
+class CharacterService
+{
+    const URL = 'https://swapi.co/api/people/';
+
+    /**
+     * @param string $search
+     * @return Character[]
+     * @throws Exception
+     */
+    public function getCharacterList($search)
+    {
+        $characterList = [];
+        $characterCollection = json_decode(ApiHelper::connect(self::URL."?search=".$search));
+        foreach($characterCollection->results as $characterObj) {
+            $character = new Character($characterObj->url);
+            $character = CharacterParser::parse($character, $characterObj);
+            $characterList[$character->getId()] = $character;
+        }
+        ksort($characterList);
+        return $characterList;
+    }
+}
